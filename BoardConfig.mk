bda8736f14320a7938a656b0d3822ffe8f7b72a7
desaishivam26 <shivamdesaixda@gmail.com>
2016-10-26 16:09:59 +0530

    osprey: Export GCC-4.8 to compile kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -29,6 +29,7 @@ TARGET_RECOVERY_DEVICE_MODULES := libinit_osprey
 
 # Kernel
 TARGET_KERNEL_CONFIG := osprey_defconfig
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := $(PWD)/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/bin/arm-eabi-
 
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216     # 16384 * 1024 mmcblk0p31
