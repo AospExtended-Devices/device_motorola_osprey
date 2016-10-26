977e7fff9321c077ccd6a893e0366401898fe9c0
desaishivam26 <shivamdesaixda@gmail.com>
2016-10-26 16:07:28 +0530

    Tesla time

--- tesla.mk
+++ tesla.mk
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 $(call inherit-product, device/motorola/osprey/full_osprey.mk)
+$(call inherit-product, vendor/tesla/config/caf_fw.mk)
 
 # Boot animation
 TARGET_SCREEN_WIDTH := 720
@@ -20,7 +21,7 @@ TARGET_SCREEN_HEIGHT := 1280
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := osprey
-PRODUCT_NAME := cm_osprey
+PRODUCT_NAME := tesla_osprey
 PRODUCT_BRAND := Motorola
 PRODUCT_MANUFACTURER := Motorola
 PRODUCT_RELEASE_NAME := osprey
