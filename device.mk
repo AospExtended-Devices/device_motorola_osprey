45051adc501c2a10bbfa6595c9fc4c4c42558ef0
GtrCraft <nickvanbokhorst@hotmail.com>
2017-01-23 10:36:34 +0000

    Aosip bringup

--- device.mk
+++ device.mk
@@ -37,3 +37,7 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 # Ramdisk
 PRODUCT_PACKAGES += \
     init.target.rc
+
+# Telephony-ext
+PRODUCT_PACKAGES += telephony-ext
+PRODUCT_BOOT_JARS += telephony-ext
