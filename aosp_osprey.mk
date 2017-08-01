5480e4f590f1bb2f51ef157dc757ddeb64c1fa1d
Alberto97 <albertop2197@gmail.com>
2017-08-01 14:59:00 +0200

    osprey: Properly set product model
    Change-Id: Id5942ec2b0b5970f23beaf56e4442406ca82fa61

--- aosp_osprey.mk
+++ aosp_osprey.mk
@@ -25,6 +25,7 @@ TARGET_SCREEN_HEIGHT := 1280
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := osprey
 PRODUCT_NAME := aosp_osprey
+PRODUCT_MODEL := MotoG3
 PRODUCT_BRAND := Motorola
 PRODUCT_MANUFACTURER := Motorola
 PRODUCT_RELEASE_NAME := osprey
