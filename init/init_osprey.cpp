f5e44f6b59afb89eaa3ee8e5805f9b16d3c20b07
Kyle Harrison <khwebmail@gmail.com>
2018-04-27 14:16:55 +0100

    osprey: init: Update overrides to build properties
    - Update description and fingerprint to fix SafetyNet mismatch
    - Use property_override_dual to override properties in vendor build.prop as well as system build.prop

--- init/init_osprey.cpp
+++ init/init_osprey.cpp
@@ -180,13 +180,12 @@ void vendor_load_properties()
         sprintf(ds, "_ds");
     }
 
-    sprintf(description, "osprey_%s%s%s%s-user 6.0.1 MPI24.107-55 33 release-keys", customerid, ds, tv, gb);
-    sprintf(fingerprint, "motorola/osprey_%s%s%s%s/%s:6.0.1/MPI24.107-55/33:user/release-keys", customerid, ds, tv, gb, device);
+    sprintf(description, "osprey_retus-user 6.0 MPI24.65-25.1 1 release-keys");
+    sprintf(fingerprint, "motorola/osprey_retus/osprey_umts:6.0/MPI24.65-25.1/1:user/release-keys");
 
-    property_override("ro.product.device", device);
-    property_override("ro.build.product", device);
-    property_override("ro.build.description", description);
-    property_override("ro.build.fingerprint", fingerprint);
-    property_override("ro.vendor.build.fingerprint", fingerprint);
+    property_override_dual("ro.product.device", "ro.vendor.product.device", device);
+    property_override_dual("ro.build.product", "ro.vendor.build.product", device);
+    property_override_dual("ro.build.description", "ro.vendor.build.description", description);
+    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", fingerprint);
     property_set("ro.mot.build.customerid", customerid);
 }
