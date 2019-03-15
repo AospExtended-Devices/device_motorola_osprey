896f479ff937696a7a60430f19fcf023c6ba749f
Shubham Singh <coolsks94@gmail.com>
2019-03-15 07:09:55 +0100

    osprey : Aex bringup
    * Add bootanimation res
    * Remove useless overlays and flags
    * Add overlays for removing unsupported LED settings
    * Refactor for aex

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,5 +14,5 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_osprey.mk
+    $(LOCAL_DIR)/aosp_osprey.mk
 
--- aosp_osprey.mk
+++ aosp_osprey.mk
@@ -16,14 +16,24 @@
 $(call inherit-product, device/motorola/osprey/full_osprey.mk)
 
 # Boot animation
-TARGET_BOOTANIMATION_HALF_RES := true
-TARGET_SCREEN_WIDTH := 720
-TARGET_SCREEN_HEIGHT := 1280
+TARGET_BOOT_ANIMATION_RES := 720
+
+# Inherit some common aosp stuff.
+$(call inherit-product, vendor/aosp/common.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := osprey
-PRODUCT_NAME := lineage_osprey
+PRODUCT_NAME := aosp_osprey
 PRODUCT_MODEL := MotoG3
 PRODUCT_BRAND := Motorola
 PRODUCT_MANUFACTURER := Motorola
 PRODUCT_RELEASE_NAME := osprey
+
+
+# Use Jelly
+TARGET_USE_JELLY := true
+
+# Use Gapps
+#WITH_GAPPS := true
+#TARGET_GAPPS_ARCH := arm
+#IS_PHONE := true
